1cd54281886a55d1bee0ec95902d87cf97fe91d5
Lucas Freitas <lucasfreitas1459@outlook.com>
2021-11-15 12:10:58 -0300

    First Commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 class="title text-white text-center rounded m-5 p-3"><img src="./assets/logo.png" alt="">  Calculator</h1>
+    <calculator></calculator>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Calculator from './components/Calculator.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Calculator
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body {
+    background: #31475e !important;
+  }
+  img {
+    width: auto;
+    height: auto;
+    max-width: 50px;
+    max-height: 50px;
+  }
 </style>
